5241767ea030146878cc2d921fb16bb6d30e9720
TheDrJosh <51982649+TheDrJosh@users.noreply.github.com>
2023-04-18 00:25:39 -0700

    program state wip

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ mod lexer;
 mod interpreter;
 mod parser;
 mod ast;
+mod program_state;
 
 fn main() {
     let mut text = String::new();
+
+    println!("Welcome To CL Calc a command line calculator tool:\nenter \"!exit\" to exit or \"!help\" for additional help.");
 
     loop {
         print!("calc> ");
@@ -15,6 +18,19 @@ fn main() {
         std::io::stdin().read_line(&mut text).unwrap();
         if text.trim().is_empty() {
             text.clear();
+            continue;
+        }
+
+        if text.trim() == "!exit" {
+            break;
+        }
+
+        if text.trim() == "!help" {
+            println!("Built in functions:\n- sqrt(x)\n- ln(x)\n- abs(x)\n- cos(x)\n- sin(x)\n- tan(x)\n- log(x)");
+            println!("You can define custom functions with name(x)=exprection.");
+            println!("Built in constants:\n- pi: {}\n- e: {}", std::f64::consts::PI, std::f64::consts::E);
+            println!("You can define custom constants with name=exprection.");
+            println!("You can enter !vars to see custom functions and constants");
             continue;
         }
 
